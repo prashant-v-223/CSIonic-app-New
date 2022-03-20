81c165795f9915fbb935e130381caa446894bb4b
divyesh-vaghela <divyeshv789@gmail.com>
2022-03-21 00:06:56 +0530

    fix: password reg exp updated with AWS allowed password policy

--- src/app/shared/validators/password-validator.ts
+++ src/app/shared/validators/password-validator.ts
@@ -1,9 +1,11 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
-// Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character (@$!%*#?&)
-export const passwordRegExp = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+// AWS password policy: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-settings-policies.html
+// Reg exp ref: https://stackoverflow.com/a/58767981
+// Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character ^ $ * . [ ] { } ( ) ? " ! @ # % & / \ , > < ' : ; | _ ~ ` = + -
+export const passwordRegExp = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.\[\]{}\(\)?\-\"!@#%&\/,><\':;|_~`])\S{8,99}$/);
 
-export const passwordRequirementMessage = 'Password must contain Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character (@$!%*#?&)';
+export const passwordRequirementMessage = 'Password must contain Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character';
 
 export function passwordValidator(control: AbstractControl): ValidationErrors | null {
   const isValidPassword = passwordRegExp.test(control.value);
