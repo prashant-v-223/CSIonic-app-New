d2b7ccad5485a5d9359453d81dc4df5101788486
Yashu1909 <64472638+Yashu1909@users.noreply.github.com>
2022-06-28 14:37:12 +0530

    notification token issue solve, notification design changes, notification redirection done

--- src/app/pages/sign-in/sign-in.page.ts
+++ src/app/pages/sign-in/sign-in.page.ts
@@ -12,6 +12,8 @@ import { UserService } from 'src/app/shared/services/user.service';
 import { passwordRequirementMessage, passwordValidator } from 'src/app/shared/validators/password-validator';
 import { ConfigurationService } from 'src/app/shared/services/configuration.service';
 import { NotificationService } from 'src/app/shared/services/notification.service';
+import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.page.html',
@@ -37,7 +39,8 @@ export class SignInPage implements OnInit {
     private toastController: ToastController,
     private userService: UserService,
     private configurationService: ConfigurationService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private platform:Platform,
   ) {}
 
   async ngOnInit() {
@@ -51,14 +54,17 @@ export class SignInPage implements OnInit {
 
   async signIn() {
     if (this.signInForm.invalid) return;
-
+    const isPushNotificationsAvailable = Capacitor.isPluginAvailable('PushNotifications');
     this.isLoading = true;
     this.signInForm.disable();
 
     try {
       await Auth.signIn(this.signInForm.value);
       await this.userService.setHeaderToken();
-      this.token = await this.notificationService.getPushNotificationToken();
+      if(isPushNotificationsAvailable)
+      {
+        this.token = await this.notificationService.getPushNotificationToken();
+      }
       const user = await this.getUser(this.token);
       const config = await this.getConfiguration();
 
