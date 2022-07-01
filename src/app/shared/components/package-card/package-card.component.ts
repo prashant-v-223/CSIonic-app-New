07306bebcd27dce6b46f98ffb922349349b8fb98
Arjun Amrutiya <71114357+ArjunAmrutiya@users.noreply.github.com>
2022-07-01 15:42:41 +0530

    setup chart in coin

--- src/app/shared/components/package-card/package-card.component.ts
+++ src/app/shared/components/package-card/package-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
+import { SIPService } from '../../services/sip.service';
 
 @Component({
   selector: 'app-package-card',
@@ -7,15 +9,22 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 })
 export class PackageCardComponent implements OnInit, OnChanges {
 
+  @ViewChild('lineCanvas') private lineCanvas: ElementRef;
+  lineChart: any;
   @Input() view: 'package'|'sip'|'coin' = 'package';
   @Input() package: any;
   @Input() navigationLink = '/';
   @Input() coinList: any;
   @Input() packageList: any;
+  coinCode: string = "";
+  chartDateLabel: any;
+  chartPriceLabel: any;
 
   coins = [];
 
-  constructor() { }
+  constructor(
+    private sipService: SIPService,
+  ) { }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.package)
@@ -30,6 +39,90 @@ export class PackageCardComponent implements OnInit, OnChanges {
   {
     this.packageList;
     this.coinList;
+    this.coinCode = this.coinList[0].currencyId.code;
+    
+    
+    
+  }
+
+  ngAfterViewInit() {
+    this.lineChartMethod(this.coinCode);
+  }
+
+  async lineChartMethod(coinCodeName:string) {
+    let coinData = await this.sipService.getChartDetails(coinCodeName,"3","month")
+    this.chartDateLabel = coinData.data.date;
+    this.chartPriceLabel = coinData.data.price;
+
+    let context: CanvasRenderingContext2D = this.lineCanvas.nativeElement.getContext('2d');
+    let grad = context.createLinearGradient(0, 0, 0, 100);
+    grad.addColorStop(0, '#2B7979');
+    grad.addColorStop(1, '#fff');
+    
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: this.chartDateLabel,
+        datasets: [
+          {
+            data: this.chartPriceLabel,
+            backgroundColor: grad,
+            borderColor: 'rgba(43, 121, 121, 1)',
+            borderCapStyle: 'butt',
+            borderWidth: 1,
+            fill: true,
+            tension: 0.5,
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#FFFFFF',
+            pointRadius: 0,
+            pointHitRadius: 5,
+            pointBorderWidth: 0.7,
+            pointHoverBorderWidth: 5,
+            pointHoverRadius: 6,
+            pointHoverBackgroundColor: '#FFFFFF',
+            pointHoverBorderColor: 'rgba(43, 121, 121, 1)',
+            spanGaps: false,
+          }
+        ]
+      },
+      options: {
+        layout: {
+          padding: {
+            bottom: 0,
+            left: 0,
+            right: 5
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          },
+          datalabels: {
+            display: false,
+          },
+        },
+        scales: {
+          y: {
+            grid: {
+              drawBorder: false,
+              display: false
+            },
+            ticks: {
+              display: false
+            }
+          },
+          x: {
+            grid: {
+              drawBorder: false,
+              display: false
+            },
+            ticks: {
+             display: false
+           }
+          }
+        }
+      }
+    });
   }
 
   populateCoins() {
